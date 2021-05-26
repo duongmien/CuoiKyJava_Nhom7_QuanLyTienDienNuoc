87bc31716336a8cfba082ab73626794406ef45b3
DESKTOP-TJSMHQ4\PC <khanhhadsdl@gmail.com>
2021-05-26 20:19:02 +0700

    Các file user

---
+++ QuanLyTienDienNuoc/src/account/KhachHang.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package account;
+
+/**
+ *
+ * @author PC
+ */
+public class KhachHang {
+    private int maKH, idGDN;
+    private String tenKH, ngaySinh, gioiTinh, sdt, doiTuong, diaChi;
+
+    public int getMaKH() {
+        return maKH;
+    }
+
+    public int getIdGDN() {
+        return idGDN;
+    }
+
+    public String getTenKH() {
+        return tenKH;
+    }
+
+    public String getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public String getDoiTuong() {
+        return doiTuong;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setMaKH(int maKH) {
+        this.maKH = maKH;
+    }
+
+    public void setIdGDN(int idGDN) {
+        this.idGDN = idGDN;
+    }
+
+    public void setTenKH(String tenKH) {
+        this.tenKH = tenKH;
+    }
+
+    public void setNgaySinh(String ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public void setDoiTuong(String doiTuong) {
+        this.doiTuong = doiTuong;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+    
+}
---
+++ QuanLyTienDienNuoc/src/account/Sotieuthu.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package account;
+
+/**
+ *
+ * @author PC
+ */
+public class Sotieuthu {
+    private int idTT, idKH;
+    private float chisocuDien, chisomoiDien, chisocuNuoc, chisomoiNuoc;
+    private String ngayNhap;
+
+    public int getIdTT() {
+        return idTT;
+    }
+
+    public int getIdKH() {
+        return idKH;
+    }
+
+    public float getChisocuDien() {
+        return chisocuDien;
+    }
+
+    public float getChisomoiDien() {
+        return chisomoiDien;
+    }
+
+    public float getChisocuNuoc() {
+        return chisocuNuoc;
+    }
+
+    public float getChisomoiNuoc() {
+        return chisomoiNuoc;
+    }
+
+    public String getNgayNhap() {
+        return ngayNhap;
+    }
+
+    public void setIdTT(int idTT) {
+        this.idTT = idTT;
+    }
+
+    public void setIdKH(int idKH) {
+        this.idKH = idKH;
+    }
+
+    public void setChisocuDien(float chisocuDien) {
+        this.chisocuDien = chisocuDien;
+    }
+
+    public void setChisomoiDien(float chisomoiDien) {
+        this.chisomoiDien = chisomoiDien;
+    }
+
+    public void setChisocuNuoc(float chisocuNuoc) {
+        this.chisocuNuoc = chisocuNuoc;
+    }
+
+    public void setChisomoiNuoc(float chisomoiNuoc) {
+        this.chisomoiNuoc = chisomoiNuoc;
+    }
+
+    public void setNgayNhap(String ngayNhap) {
+        this.ngayNhap = ngayNhap;
+    }
+    
+    
+}
